d865b5f1fb478b3937e2fac9b65bf586da26ff23
vaddebalu <vadde.balu@gmail.com>
2023-11-10 17:25:20 +0530

    How to use groupByKey,reduceBykey,aggregateByKey & sortByKey in PySparkRDD

---
+++ transformations/aggregates/reduceByDemo.py
@@ -0,0 +1,12 @@
+"""
+This PySpark program shows how to apply groupbykey using RDD
+"""
+from operator import add
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName('Test').setMaster('local[*]')
+sc=SparkContext(conf=conf)
+distData = sc.parallelize([23,12,11,552,22,66,77,88,23,555,64,6555,54,6871,6874,434,5,4,54,54,4,4,54,546,6445])
+mapped=distData.map(lambda x:(x,1))
+#apply reduceByKey using add operator
+numcount=mapped.reduceByKey(add)
+numcount.saveAsTextFile('num50')
--- transformations/aggregates/sortByKey.py
+++ transformations/aggregates/sortByKey.py
@@ -1,11 +1,11 @@
 """
-This PySpark program shows how to apply groupbykey using RDD
+This PySpark program shows how to apply sortByKey using RDD
 """
 from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName('Test').setMaster('local[*]')
 sc=SparkContext(conf=conf)
 distData = sc.parallelize([23,12,11,552,22,66,77,88,23,555,64,6555,54,6871,6874,434,5,4,54,54,4,4,54,546,6445])
 mapped=distData.map(lambda x:(x,1))
-#apply groupByKey
-numcount=mapped.groupByKey().mapValues(len)
-numcount.saveAsTextFile('num50')
+#apply reduceByKey using add operator
+sorted=mapped.sortByKey()
+sorted.saveAsTextFile('num50')
