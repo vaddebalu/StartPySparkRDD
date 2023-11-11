6b965d772d7f6dc91affbfa4d537d8df7c17d5d6
vaddebalu <vadde.balu@gmail.com>
2023-11-11 15:00:09 +0530

    Convert RDD to DataFrame in PySpark

--- Basic/RDD2DF.py
+++ Basic/RDD2DF.py
@@ -1,15 +1,19 @@
 """
 convert RDD into dataframe using toDF() function in PySpark
 """
-from pyspark.sql.types import StructType,StructField,StringType
+from pyspark.sql.types import StructType,StructField,IntegerType
 from pyspark import SparkContext, SparkConf
+from pyspark.sql.types import StringType
 from pyspark.sql import SparkSession
 #define schema
-numsSchema=StructType([StructField('num',StringType(),True)])
-
-#
 
 spark=SparkSession.builder.getOrCreate()
 nums = spark.sparkContext.parallelize([23,12,11,552,22,66,77,88,23,555,64,6555,54,6871,6874,434,5,4,54,54,4,4,54,546,6445])
-numsdf2=nums.toDF(schema=numsSchema)
-numsdf2.printSchema()
+
+
+numsSchema=StructType([StructField('num',IntegerType(),True)])
+nums=nums.map(lambda x:(x,))
+#convert nums rdd to dataframe
+spark=SparkSession.builder.getOrCreate()
+numsDF=spark.createDataFrame(nums,schema=numsSchema)
+numsDF.show()
